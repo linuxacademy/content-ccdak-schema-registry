f1805df2525d517e5326e102824feaef022c43dd
Will Boyd <shazzenbot@gmail.com>
2019-09-03 16:04:38 -0400

    init

---
+++ src/main/java/com/linuxacademy/ccdak/schemaregistry/SchemaRegistryConsumerMain.java
@@ -0,0 +1,9 @@
+package com.linuxacademy.ccdak.schemaregistry;
+
+public class SchemaRegistryConsumerMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
---
+++ src/main/java/com/linuxacademy/ccdak/schemaregistry/SchemaRegistryProducerMain.java
@@ -0,0 +1,9 @@
+package com.linuxacademy.ccdak.schemaregistry;
+
+public class SchemaRegistryProducerMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
