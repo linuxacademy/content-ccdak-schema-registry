3bfdcbe397981ee04e3027b6de767daaa4f824be
Will Boyd <shazzenbot@gmail.com>
2019-09-04 09:43:02 -0400

    add end atate

--- src/main/java/com/linuxacademy/ccdak/schemaregistry/SchemaRegistryConsumerMain.java
+++ src/main/java/com/linuxacademy/ccdak/schemaregistry/SchemaRegistryConsumerMain.java
@@ -1,9 +1,42 @@
 package com.linuxacademy.ccdak.schemaregistry;
 
+import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
+import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
 public class SchemaRegistryConsumerMain {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        final Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "group1");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
+        props.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, true); 
+
+        KafkaConsumer<String, Person> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singletonList("employees"));
+
+        while (true) {
+            final ConsumerRecords<String, Person> records = consumer.poll(Duration.ofMillis(100));
+            for (final ConsumerRecord<String, Person> record : records) {
+                final String key = record.key();
+                final Person value = record.value();
+                System.out.println("key=" + key + ", value=" + value);
+            }
+        }
     }
 
 }
--- src/main/java/com/linuxacademy/ccdak/schemaregistry/SchemaRegistryProducerMain.java
+++ src/main/java/com/linuxacademy/ccdak/schemaregistry/SchemaRegistryProducerMain.java
@@ -1,9 +1,33 @@
 package com.linuxacademy.ccdak.schemaregistry;
 
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
 public class SchemaRegistryProducerMain {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        final Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.RETRIES_CONFIG, 0);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
+
+        KafkaProducer<String, Person> producer = new KafkaProducer<String, Person>(props);
+        
+        Person kenny = new Person(125745, "Kenny", "Armstrong", "kenny@linuxacademy.com");
+        producer.send(new ProducerRecord<String, Person>("employees", kenny.getId().toString(), kenny));
+        
+        Person terry = new Person(943256, "Terry", "Cox", "kenny@linuxacademy.com");
+        producer.send(new ProducerRecord<String, Person>("employees", terry.getId().toString(), terry));
+        
+        producer.close();
     }
 
 }
